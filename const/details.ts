3db23f9392ff4ee26de5a12e8cf2e60f0a757397
unbanksytv <unbanksytv@gmail.com>
2023-06-30 23:20:16 +0200

    first commit

---
+++ const/details.ts
@@ -0,0 +1,6 @@
+import { Chain, Mumbai } from "@thirdweb-dev/chains";
+
+export const TOKEN_ADDRESS: string =
+  "0x52f98feE8D7A5f3045906fBD6d801BB93Ed250E7";
+export const DEX_ADDRESS: string = "0xD0797fe9bfb144af4d7Fd48F997a5c1bAE896cdf";
+export const ACTIVE_CHAIN: Chain = Mumbai;
